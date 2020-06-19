4042f65110536d89f6b9fc56d8203a560ef2f132
krebikshaw <krebikshaw@gmail.com>
2020-06-19 11:16:11 +0800

    hw1 ~ hw5 完成

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,17 @@
 function capitalize(str) {
-  
+	var result = ""
+	if(str[0] >= "a" && str[0] < "z"){
+		result += str[0].toUpperCase()
+	} else {
+		result += str[0]
+	}
+	for(var i=1; i<str.length; i++){
+		result += str[i]
+	}
+	return result
 }
 
 console.log(capitalize('hello'));
+
+
+
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,19 @@
 function join(arr, concatStr) {
-  
+	var result = arr[0].toString()
+	for(var i=1; i<arr.length; i++){
+		result += concatStr
+		result += arr[i]
+	}  
+	return result
 }
 
 function repeat(str, times) {
-  
+	var result = ""
+	for(var i=0; i<times; i++){
+		result += str
+	}
+	return result
 }
 
-console.log(join(['a'], '!'));
+console.log(join(["a"], '!'));
 console.log(repeat('a', 5));
