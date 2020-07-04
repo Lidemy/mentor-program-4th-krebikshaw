c0fcf1818eca5c3b1106d35a71fef793ae1ef1f3
krebikshaw <krebikshaw@gmail.com>
2020-07-04 15:52:00 +0800

    hw1~hw3 update

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -1,11 +1,22 @@
 /* eslint-disable no-plusplus, no-use-before-define, no-shadow */
 const request = require('request');
 
-const URL = 'https://lidemy-book-store.herokuapp.com/books?limit=10';
+const URL = 'https://lidemy-book-store.herokuapp.com/books?_limit=10';
 
 request.get(URL, (error, response, body) => {
-  const content = JSON.parse(body);
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  let content;
+  try {
+    content = JSON.parse(body);
+  } catch (e) {
+    console.log(e);
+  }
+
   for (let i = 0; i < content.length; i++) {
-    console.log(`${content[i].id} ${content[i].name}`);
+    console.log(content[i].id, content[i].name);
   }
 });
