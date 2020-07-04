fbf0883e924f9a7f6f3fe93997985cae878b9ff0
krebikshaw <krebikshaw@gmail.com>
2020-07-04 15:52:00 +0800

    hw1~hw3 update

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -1,76 +1,57 @@
 /* eslint-disable no-plusplus, no-use-before-define, no-shadow, object-shorthand,
 camelcase, prefer-destructuring, function-paren-newline, comma-dangle */
 const request = require('request');
+const requestPromise = require('request-promise');
 
 const argv = process.argv;
+const baseUrl = 'https://lidemy-book-store.herokuapp.com/books';
+const num = 20;
+const method = argv[2];
+const firstKey = argv[3];
+const secondKey = argv[4];
 
-function start() {
-  if (!argv[3]) {
-    request.get('https://lidemy-book-store.herokuapp.com/books?_limit=20',
-      (error, response, body) => {
-        const content = JSON.parse(body);
-        for (let i = 0; i < content.length; i++) {
-          console.log(`${content[i].id} ${content[i].name}`);
-        }
-      }
-    );
-    return;
+function getData(body) {
+  let data;
+  try {
+    data = JSON.parse(body);
+  } catch (e) {
+    console.log(e);
   }
-
-  if (argv[4]) {
-    const input_id = argv[argv.length - 2];
-    const input_name = argv[argv.length - 1];
-    request.patch(`https://lidemy-book-store.herokuapp.com/books/${input_id}`,
-      {
-        form: { name: input_name }
-      }
-    );
-    console.log(`你已將 id = ${input_id} 的書名更改成 ${input_name}！`);
-    return;
-  }
-
-  const input_method = argv[argv.length - 2];
-  const input_id = argv[argv.length - 1];
-
-  switch (input_method) {
-    case 'read':
-      doRead(input_id);
-      break;
-    case 'delete':
-      doDelete(input_id);
-      break;
-    case 'create':
-      doCreate(input_id);
-      break;
-    default:
-      console.log('輸入內容有誤喔！請再試一次。');
-  }
-
-  function doRead(id) {
-    request.get(`https://lidemy-book-store.herokuapp.com/books/${id}`,
-      (error, response, body) => {
-        const content = JSON.parse(body);
-        console.log(content.name);
-      }
-    );
-  }
-
-  function doDelete(id) {
-    request.delete(`https://lidemy-book-store.herokuapp.com/books/${id}`,
-      () => {
-        console.log(`你已將 id = ${id} 的書成功刪除囉！`);
-      }
-    );
-  }
-
-  function doCreate(name) {
-    request.post('https://lidemy-book-store.herokuapp.com/books',
-      {
-        form: { name: name }
-      }
-    );
-    console.log(`已成功新增 ${name}！`);
+  if (data.length > 1) {
+    data.map(element => console.log(element.id, element.name));
+  } else {
+    console.log(data.id, data.name);
   }
 }
 
-start();
+if (method === 'list') {
+  requestPromise(`${baseUrl}?_limit=${num}`)
+    .then(htmlString => getData(htmlString))
+    .catch(error => console.log(error));
+}
+
+if (method === 'read') {
+  if (!firstKey) console.log('請輸入書本 id');
+  requestPromise(`${baseUrl}/${firstKey}`)
+    .then(htmlString => getData(htmlString))
+    .catch(error => console.log(error));
+}
+
+if (method === 'delete') {
+  if (!firstKey) console.log('請輸入書本 id');
+  request.delete(`${baseUrl}/${firstKey}`);
+  console.log(`你已將 id = ${firstKey} 的書本成功刪除囉！`);
+}
+
+if (method === 'create') {
+  if (!firstKey) console.log('請輸入書名');
+  request.post(`${baseUrl}`).form({ name: firstKey });
+  console.log(`已成功新增 ${firstKey}！`);
+}
+
+if (method === 'update') {
+  if (!firstKey) console.log('請輸入書本 id');
+  if (!secondKey) console.log('請輸入要更改的書名');
+  request.patch(`${baseUrl}/${firstKey}`).form({ name: secondKey });
+  console.log(`你已將 id = ${firstKey} 的書名更改成 ${secondKey}！`);
+}
