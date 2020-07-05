48d28ed82136fdc6e20cb3cf327b1507eb5b8b94
krebikshaw <krebikshaw@gmail.com>
2020-07-05 19:32:00 +0800

    save

---
+++ homeworks/week4/challenage.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-plusplus, no-use-before-define, no-shadow, function-paren-newline,
+comma-dangle, prefer-destructuring */
+
+/*
+const request = require('request');
+const requestPromise = require('request-promise');
+
+const argv = process.argv;
+const inputName = argv[2];
+const userId = 't1i56f8yeobnh6vqwymu1jo3tkhtlp';
+const baseUrl = 'https://api.twitch.tv/kraken/streams/';
+
+const options = {
+  method: 'GET',
+  url: `${baseUrl}?game=${inputName}`,
+  headers: {
+    'Client-ID': userId,
+    Authorization: 'Bearer cfabdegwdoklmawdzdo98xt2fo512y',
+    Accept: 'application/vnd.twitchtv.v5+json',
+  },
+}
+
+function callback(error, response, body) {
+  console.log(JSON.parse(body))
+}
+
+request(options, callback)
+*/
