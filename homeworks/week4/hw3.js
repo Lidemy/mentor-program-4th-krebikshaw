18772cd123c6e02ee833df98238c328664197889
krebikshaw <krebikshaw@gmail.com>
2020-07-07 00:22:30 +0800

    hw3 revise

--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -7,7 +7,10 @@ const keyWord = argv[2];
 
 request.get(`https://restcountries.eu/rest/v2/name/${keyWord}`,
   (error, response, body) => {
-    if (response.statusCode === 404) console.log('找不到國家資訊');
+    if (response.statusCode === 404) {
+      console.log('找不到國家資訊');
+      return;
+    }
     if (error) {
       console.log('查詢失敗');
       return;
@@ -18,6 +21,7 @@ request.get(`https://restcountries.eu/rest/v2/name/${keyWord}`,
       data = JSON.parse(body);
     } catch (e) {
       console.log('查詢失敗');
+      return;
     }
 
     for (let i = 0; i < data.length; i++) {
